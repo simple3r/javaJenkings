0b42726c86efbc8e32420a969ab97a39117a73b9
luis <ladpaneque@gmail.com>
2024-04-07 19:17:51 -0600

    update project

---
+++ src/main/java/com/main/jenkingsJava/JenkingsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.main.jenkingsJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkingsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkingsJavaApplication.class, args);
+	}
+
+}
